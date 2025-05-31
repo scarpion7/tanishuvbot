41acee8d0a6662704eb2869c6884913a0f675edb
scarpion7 <silicatasman7@gmail.com>
2025-05-31 14:46:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -621,7 +621,8 @@ async def process_gender(callback_query: CallbackQuery, state: FSMContext):
     await state.update_data(gender=GENDER_OPTIONS[gender_key][lang], gender_key=gender_key)
 
     await state.set_state(Form.country)
-    await callback_query.message.edit_text(TEXTS[lang]["country_prompt"])
+    
+    await callback_query.message.answer(TEXTS[lang]["country_prompt"], reply_markup=get_country_keyboard(lang))
     await callback_query.answer()
 
 @dp.callback_query(Form.country, F.data.startswith("country_"))
